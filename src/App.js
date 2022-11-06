c3d7be9894844f7742b9631e02766ab7111dd9b5
moaath-sh <moaathsh16@gmail.com>
2022-11-06 21:30:51 +0200

    route_task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+
+import { BrowserRouter,Route, Routes,NavLink } from 'react-router-dom';
 import './App.css';
+import Home from './Home';
+import Contact from './Contact';
+import About from './About';
+import Service from './Service';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>React router Task</h1>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/Contact">Contact</NavLink>
+        <NavLink to="/About">About</NavLink>
+        <NavLink to="/Service">Service</NavLink>
+        <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/Contact' element={<Contact />}/>
+        <Route path='/About' element={<About />}/>
+        <Route path='/Service' element={<Service />}/>
+        </Routes>
+        
+      </div>
+    </BrowserRouter>
+    
   );
 }
 
